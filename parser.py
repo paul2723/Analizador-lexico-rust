7b21815ba5e3c064091dd4040c0e3e7fe99f8ce0
paul123 <139300207+paul2723@users.noreply.github.com>
2026-06-26 22:51:17 +0000

    declaraciones, tipos de datos, estructuras,arreglos, tuplas, vectores y llamadas/rutas

--- parser.py
+++ parser.py
@@ -163,3 +163,226 @@ def p_ciclo_for(p):
 def p_ciclo_loop(p):
     'ciclo_loop : LOOP bloque'
     p[0] = ('loop', p[2])
+
+# ================================================================
+# APORTE 2 - Alcivar Zavala Paul
+# Responsabilidad: declaraciones, tipos de datos, estructuras,
+# arreglos, tuplas, vectores y llamadas/rutas.
+# ================================================================
+
+def p_declaracion_variable(p):
+    'declaracion_variable : LET mutabilidad_opt IDENTIFICADOR tipo_opt inicializacion_opt'
+    p[0] = ('let', p[2], p[3], p[4], p[5])
+
+
+def p_mutabilidad_opt(p):
+    '''mutabilidad_opt : MUT
+                       | vacio'''
+    p[0] = True if p[1] == 'mut' else False
+
+
+def p_tipo_opt(p):
+    '''tipo_opt : DOS_PUNTOS tipo
+                | vacio'''
+    p[0] = p[2] if len(p) == 3 else None
+
+
+def p_inicializacion_opt(p):
+    '''inicializacion_opt : ASIGNACION expresion
+                          | ASIGNACION struct_literal
+                          | vacio'''
+    p[0] = p[2] if len(p) == 3 else None
+
+
+def p_tipo(p):
+    '''tipo : tipo_primitivo
+            | IDENTIFICADOR
+            | AMPERSAND tipo
+            | CORCHETE_IZQ tipo PUNTO_COMA NUMERO_ENTERO CORCHETE_DER
+            | PARENTESIS_IZQ lista_tipos_opt PARENTESIS_DER
+            | TIPO_VEC MENOR tipo MAYOR
+            | TIPO_OPTION MENOR tipo MAYOR
+            | TIPO_RESULT MENOR tipo COMA tipo MAYOR'''
+    if len(p) == 2:
+        p[0] = ('tipo', p[1])
+    elif len(p) == 3:
+        p[0] = ('referencia_tipo', p[2])
+    elif len(p) == 4:
+        p[0] = ('tupla_tipo', p[2])
+    elif len(p) == 5:
+        p[0] = ('generico', p[1], p[3])
+    elif len(p) == 6:
+        p[0] = ('array_tipo', p[2], p[4])
+    else:
+        p[0] = ('result_tipo', p[3], p[5])
+
+
+def p_tipo_primitivo(p):
+    '''tipo_primitivo : TIPO_I8
+                      | TIPO_I16
+                      | TIPO_I32
+                      | TIPO_I64
+                      | TIPO_I128
+                      | TIPO_U8
+                      | TIPO_U16
+                      | TIPO_U32
+                      | TIPO_U64
+                      | TIPO_U128
+                      | TIPO_F32
+                      | TIPO_F64
+                      | TIPO_BOOL
+                      | TIPO_CHAR
+                      | TIPO_STR
+                      | TIPO_STRING
+                      | TIPO_USIZE
+                      | TIPO_ISIZE
+                      | TIPO_VEC
+                      | TIPO_OPTION
+                      | TIPO_RESULT
+                      | TIPO_HASHMAP'''
+    p[0] = p[1]
+
+
+def p_lista_tipos_opt(p):
+    '''lista_tipos_opt : lista_tipos
+                       | vacio'''
+    p[0] = p[1]
+
+
+def p_lista_tipos(p):
+    '''lista_tipos : lista_tipos COMA tipo
+                   | tipo'''
+    if len(p) == 4:
+        p[0] = p[1] + [p[3]]
+    else:
+        p[0] = [p[1]]
+
+
+def p_estructura(p):
+    'estructura : STRUCT IDENTIFICADOR LLAVE_IZQ campos_struct_opt LLAVE_DER'
+    p[0] = ('struct', p[2], p[4])
+
+
+def p_campos_struct_opt(p):
+    '''campos_struct_opt : campos_struct
+                         | vacio'''
+    p[0] = p[1]
+
+
+def p_campos_struct(p):
+    '''campos_struct : campos_struct campo_struct
+                     | campo_struct'''
+    if len(p) == 3:
+        p[0] = p[1] + [p[2]]
+    else:
+        p[0] = [p[1]]
+
+
+def p_campo_struct(p):
+    'campo_struct : IDENTIFICADOR DOS_PUNTOS tipo coma_opt'
+    p[0] = ('campo', p[1], p[3])
+
+
+def p_struct_literal(p):
+    'struct_literal : IDENTIFICADOR LLAVE_IZQ pares_campo_opt LLAVE_DER'
+    p[0] = ('struct_literal', p[1], p[3])
+
+
+def p_pares_campo_opt(p):
+    '''pares_campo_opt : pares_campo
+                       | vacio'''
+    p[0] = p[1]
+
+
+def p_pares_campo(p):
+    '''pares_campo : pares_campo par_campo
+                   | par_campo'''
+    if len(p) == 3:
+        p[0] = p[1] + [p[2]]
+    else:
+        p[0] = [p[1]]
+
+
+def p_par_campo(p):
+    'par_campo : IDENTIFICADOR DOS_PUNTOS expresion coma_opt'
+    p[0] = ('par_campo', p[1], p[3])
+
+
+def p_coma_opt(p):
+    '''coma_opt : COMA
+                | vacio'''
+    p[0] = None
+
+
+def p_enumeracion(p):
+    'enumeracion : ENUM IDENTIFICADOR LLAVE_IZQ variantes_enum_opt LLAVE_DER'
+    p[0] = ('enum', p[2], p[4])
+
+
+def p_variantes_enum_opt(p):
+    '''variantes_enum_opt : variantes_enum
+                          | vacio'''
+    p[0] = p[1]
+
+
+def p_variantes_enum(p):
+    '''variantes_enum : variantes_enum variante_enum
+                      | variante_enum'''
+    if len(p) == 3:
+        p[0] = p[1] + [p[2]]
+    else:
+        p[0] = [p[1]]
+
+
+def p_variante_enum(p):
+    '''variante_enum : IDENTIFICADOR coma_opt
+                     | IDENTIFICADOR PARENTESIS_IZQ lista_tipos_opt PARENTESIS_DER coma_opt'''
+    p[0] = ('variante', p[1])
+
+
+def p_implementacion(p):
+    'implementacion : IMPL IDENTIFICADOR LLAVE_IZQ sentencias_opt LLAVE_DER'
+    p[0] = ('impl', p[2], p[4])
+
+
+def p_llamada_funcion(p):
+    '''llamada_funcion : IDENTIFICADOR PARENTESIS_IZQ argumentos_opt PARENTESIS_DER
+                       | ruta PARENTESIS_IZQ argumentos_opt PARENTESIS_DER'''
+    p[0] = ('llamada', p[1], p[3])
+
+
+def p_llamada_macro(p):
+    '''llamada_macro : IDENTIFICADOR NOT_LOGICO PARENTESIS_IZQ argumentos_opt PARENTESIS_DER
+                     | IDENTIFICADOR NOT_LOGICO CORCHETE_IZQ argumentos_opt CORCHETE_DER'''
+    p[0] = ('macro', p[1], p[4])
+
+
+def p_ruta(p):
+    '''ruta : base_ruta DOBLE_DOS_PUNTOS IDENTIFICADOR
+            | ruta DOBLE_DOS_PUNTOS IDENTIFICADOR'''
+    p[0] = ('ruta', p[1], p[3])
+
+
+def p_base_ruta(p):
+    '''base_ruta : IDENTIFICADOR
+                 | tipo_primitivo
+                 | SELF
+                 | SELF_TYPE
+                 | SUPER
+                 | CRATE'''
+    p[0] = p[1]
+
+
+def p_argumentos_opt(p):
+    '''argumentos_opt : argumentos
+                      | vacio'''
+    p[0] = p[1]
+
+
+def p_argumentos(p):
+    '''argumentos : argumentos COMA expresion
+                  | expresion'''
+    if len(p) == 4:
+        p[0] = p[1] + [p[3]]
+    else:
+        p[0] = [p[1]]
